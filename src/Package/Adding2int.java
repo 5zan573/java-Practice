21f056472892935a3b65052f620d68c006e09c03
5zan573 <faizanhussain573@gmail.com>
2025-02-05 12:52:46 +0530

    practice

---
+++ src/Package/Adding2int.java
@@ -0,0 +1,30 @@
+package Package;
+
+public class Adding2int {
+
+	public static void main(String[] args) {
+		
+	int a = 10;
+	int b = 20;
+	
+	int c = a+b;
+	
+	System.out.println(c);
+	
+	Adding2int O1 = new Adding2int();
+	int f = O1.add(50, 10);
+	
+	System.out.println(f);
+	}
+			public int add (int d, int e) {
+				
+				int f = d+e;
+				
+				return f;
+			}
+			
+		}
+
+	
+
+
---
+++ src/Package/appendStringBuffer.java
@@ -0,0 +1,14 @@
+package Package;
+
+public class appendStringBuffer {
+
+	public static void main(String[] args) {
+		
+		StringBuffer a = new StringBuffer("Faizan");
+		
+		a.append(" ").append("Hussain");
+        
+		System.out.println(a);
+	}
+
+}
