4959aaba3bb03a52338314042937a5a8926ed4c8
5zan573 <faizanhussain573@gmail.com>
2025-02-09 20:05:58 +0530

    Practice Please enter the commit message for your changes. Lines starting

---
+++ src/Package/DifferenceInLength.java
@@ -0,0 +1,19 @@
+package Package;
+
+public class DifferenceInLength {
+
+	public static void main(String[] args) {
+		
+		int[] a = {1,2,3,4,5};
+		
+		System.out.println(a.length);
+		
+		String b = "Faizan";
+		
+		System.out.println(b.length());
+	}
+
+}
+
+//* lenght = No parentheses () because length is a variable.//*
+//* lenght() = Uses () because length() is a method.//*
---
+++ src/Package/startsWith.java
@@ -0,0 +1,19 @@
+package Package;
+
+public class startsWith {
+
+	public static void main(String[] args) {
+		
+		
+		String a = "Faizan Hussain";
+		
+		System.out.println(a.startsWith("Fai",0));
+		System.out.println(a.startsWith("zan", 3));
+		System.out.println(a.startsWith("Hus", 7));
+
+		
+		//endsWith()also same as startsWith()
+
+	}
+
+}
