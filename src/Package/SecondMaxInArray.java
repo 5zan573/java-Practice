cffbfb6dac55b9b4e42382eb33075c173d8856f7
5zan573 <faizanhussain573@gmail.com>
2025-04-25 20:50:31 +0530

    practice

---
+++ src/Package/SecondMaxInArray.java
@@ -0,0 +1,25 @@
+package Package;
+
+public class SecondMaxInArray {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 2, 3, 4, 5, 6, 34, 35, };
+
+		int max = 0;
+		int secmax = 0;
+
+		for (int num : arr) {
+			if (num > max) {
+				secmax = max;
+				max = num;
+			}
+
+			else if (num > secmax && num != max) {
+				secmax = num;
+			}
+		}
+		System.out.println(secmax);
+	}
+
+}
