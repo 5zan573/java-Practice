84b6bf324c7308b93cf0d37ab3fc8790198ca232
5zan573 <faizanhussain573@gmail.com>
2025-05-05 22:54:46 +0530

    practice

--- src/Package/ReverseInteger.java
+++ src/Package/ReverseInteger.java
@@ -1,27 +1,18 @@
 package Package;
 
-import java.util.Scanner;
-
 public class ReverseInteger {
 
 	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("Enter an integer: ");
-		int num = scanner.nextInt();
-		
+		int a = 12345;
 		int rev = 0;
-		
-		while (num !=0) {
-			rev = rev * 10 + num % 10;
-			num = num /10;
+
+		while (a != 0) {
+
+			rev = rev * 10 + a % 10;
+			a = a / 10;
 		}
-		System.out.println("Reversed Num is " +rev);
-
-		scanner.close();
-		
-
+		System.out.println(rev);
 	}
 
 }
