a3f2afeaf8c779181211966b9d7d55c6ce428d21
5zan573 <faizanhussain573@gmail.com>
2025-07-16 23:10:24 +0530

    practice

---
+++ src/Package/StringConcat.java
@@ -0,0 +1,14 @@
+package Package;
+
+public class StringConcat {
+
+	public static void main(String[] args) {
+		
+		String s = "Hello";
+		s=s.toUpperCase();
+		s=s.concat(" World");
+		System.out.println(s);
+
+	}
+
+}
