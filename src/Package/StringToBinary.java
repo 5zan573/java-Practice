3f181e01244fe56e2be8a1739cda850b59d08fce
5zan573 <faizanhussain573@gmail.com>
2025-08-04 22:51:19 +0530

    practice

---
+++ src/Package/StringToBinary.java
@@ -0,0 +1,15 @@
+package Package;
+
+public class StringToBinary {
+
+	public static void main(String[] args) {
+		
+		String str = "10";
+		
+		int a = Integer.parseInt(str, 2);
+		
+		System.out.println(a);
+
+	}
+
+}
